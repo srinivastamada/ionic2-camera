644046c69976c53c31dd8ea46a254f65d4f75392
Srinivas Tamada <srinivas_tamada@homedepot.com>
2017-03-30 10:06:57 -0400

    Array Revers

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 import {Camera, CameraOptions} from '@ionic-native/camera';
+
 //import {CameraService} from '../../app/services/camera.service';
 
 @Component({selector: 'page-camera', templateUrl: 'camera.html'})
 export class CameraPage {
   photos : any;
   public base64Image : string;
-  constructor(public navCtrl : NavController, private camera: Camera) {}
+  constructor(public navCtrl : NavController, private camera : Camera, private alertCtrl : AlertController) {}
 
   ngOnInit() {
     this.photos = [];
@@ -15,43 +16,52 @@ export class CameraPage {
 
   deletePhoto(index) {
 
-    this
-      .photos
-      .splice(index, 1);
-   
-   
-    //return this.photos; return this.photos.slice(index, 1);
+    let confirm = this
+      .alertCtrl
+      .create({
+        title: 'Use this lightsaber?',
+        message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+        buttons: [
+          {
+            text: 'Disagree',
+            handler: () => {
+              console.log('Disagree clicked');
+              return false;
+            }
+          }, {
+            text: 'Agree',
+            handler: () => {
+              console.log('Agree clicked');
+              this
+                .photos
+                .splice(index, 1);
+              return true;
+            }
+          }
+        ]
+      });
+    confirm.present();
   }
-  // takePhotox() {
-  //   console.log("this.cameraSerive.takePhoto()");
-  //   console.log(this.cameraSerive.takePhoto());
-
-  //   if (this.cameraSerive.takePhoto()) {
-  //     this
-  //       .photos
-  //       .push(this.cameraSerive.takePhoto());
-  //   }
-
-  //   console.log("this.photos.length");
-  //   alert(this.photos.length);
-
-  // }
 
   takePhoto() {
-   const options : CameraOptions = {
+    const options : CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
 
-    this.camera
+    this
+      .camera
       .getPicture(options)
       .then((imageData) => {
         this.base64Image = "data:image/jpeg;base64," + imageData;
         this
           .photos
           .push(this.base64Image);
+        this
+          .photos
+          .reverse();
       }, (err) => {
         console.log(err);
       });
